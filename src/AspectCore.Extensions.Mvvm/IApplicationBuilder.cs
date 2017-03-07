2198032472399980f3dd6fe1d90352a9d76e59c5
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-03-07 17:22:34 +0800

    add new interface

---
+++ src/AspectCore.Extensions.Mvvm/IApplicationBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspectCore.Extensions.Mvvm
+{
+    public interface IApplicationBuilder
+    {
+    }
+}
--- src/AspectCore.Extensions.Mvvm/IApplicationStartup.cs
+++ src/AspectCore.Extensions.Mvvm/IApplicationStartup.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AspectCore.Extensions.Mvvm.DependencyInjection;
 
 namespace AspectCore.Extensions.Mvvm
 {
     public interface IApplicationStartup
     {
-        IServiceProvider ConfigureServices();
+        void ConfigureServices(IServiceRegister register);
 
-        void Configure();
+        void ConfigureApplication(IApplicationBuilder app);
     }
 }
