62bc10e13a32d9409cb9c318f1c76efb8d2771d1
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-03-07 23:37:08 +0800

    add Bootstrapper.cs

---
+++ src/AspectCore.Extensions.Mvvm/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspectCore.Extensions.Mvvm
+{
+    public class Bootstrapper
+    {
+        public void Startup<IApplicationStartup>()
+        {
+        }
+    }
+}
