8e1b8da36e647a4c0c9245440bddcc5c66716600
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-03-13 23:48:30 +0800

    add ServiceRegister

---
+++ src/AspectCore.Extensions.Mvvm/DependencyInjection/ServiceRegister.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspectCore.Extensions.Mvvm.DependencyInjection
+{
+    internal sealed class ServiceRegister : IServiceRegister
+    {
+        private readonly ICollection<RegistryEntry> collection = new List<RegistryEntry>();
+
+        public void Add(RegistryEntry entry)
+        {
+            if (entry == null)
+            {
+                throw new ArgumentNullException(nameof(entry));
+            }
+            collection.Add(entry);
+        }
+
+        public IEnumerator<RegistryEntry> GetEnumerator()
+        {
+            return collection.ToList().GetEnumerator();
+        }
+
+        public void Replace(RegistryEntry entry)
+        {
+            if (entry == null)
+            {
+                throw new ArgumentNullException(nameof(entry));
+            }
+            var existingEntry = collection.FirstOrDefault(x => x.ServiceType == entry.ServiceType);
+            collection.Remove(existingEntry);
+            collection.Add(entry);
+        }
+    }
+}
